59f45ff1b0eac0ae3e8ec746cb56f558737ebd95
Arkadiusz Lipiecki <arkadiusz.lipiecki@pwr.edu.pl>
2026-04-19 13:32:49 +0200

    fix issue with duplicate column names

--- src/windows/DataLoaders.py
+++ src/windows/DataLoaders.py
@@ -14,8 +14,8 @@ class EnergyDataLoader:
             case 'DE':
                 self.frame = (
                     polars.scan_csv(f'{source}/{market}.csv', separator=',', has_header=True, infer_schema_length=100_000)
-                    .select(
-                        polars.col([date_col, hour_col, main_col, *internals]),
+                    .select(polars.col([date_col, hour_col, main_col, *internals]))
+                    .with_columns(
                         polars.all().exclude([date_col, hour_col, main_col]).fill_nan(0),
                         *[polars.sum_horizontal(polars.col(map_from)).alias(map_to) for map_to, map_from in mappings.items()],
                         polars.col(date_col).cast(polars.String).str.to_date('%Y%m%d').dt.weekday().alias('weekday')
@@ -25,7 +25,9 @@ class EnergyDataLoader:
                         .select(
                             polars.col(date_col),
                             polars.col(hour_col),
-                            polars.col(externals),
+                            polars.col(externals)
+                        ).with_columns(
+                            polars.all().exclude([date_col, hour_col]).fill_nan(0)
                         ), on={date_col, hour_col}, how='inner'
                     )
                 ).filter(
@@ -39,8 +41,8 @@ class EnergyDataLoader:
         self.main_col = main_col
         self.date_col = date_col
         self.hour_col = hour_col
-        self.internals = internals + list(mappings.keys())
-        self.externals = externals
+        self.internals = [*(internals + list(mappings.keys()))]
+        self.externals = [*externals]
 
     def get(self):
         return self.frame
