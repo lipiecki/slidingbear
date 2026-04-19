79ab19f9caff6e53c71705ed60bd40f164f3898a
Arkadiusz Lipiecki <arkadiusz.lipiecki@pwr.edu.pl>
2026-04-19 15:47:02 +0200

    correct fill_nan

--- src/windows/DataLoaders.py
+++ src/windows/DataLoaders.py
@@ -6,23 +6,23 @@ class EnergyDataLoader:
                  internals: list[str] = None, 
                  externals: list[str] = None, 
                  mappings: dict[str, list] = None,
-                 main_col = "price",
-                 date_col = "date",
-                 hour_col = "hour",
-                 fill_nan: bool = True):
+                 main_col: str = "price",
+                 date_col: str = "date",
+                 hour_col: str = "hour",
+                 fill_nan = 0):
         
         self.frame = (
             polars.scan_csv(f'{source}/{market}.csv', separator=',', has_header=True, infer_schema_length=100_000)
             .select(polars.col([date_col, hour_col, main_col, *internals]))
             .with_columns(
-                polars.all().exclude([date_col, hour_col, main_col]).fill_nan(0),
+                polars.all().exclude([date_col, hour_col, main_col]).fill_nan(fill_nan),
                 *[polars.sum_horizontal(polars.col(map_from)).alias(map_to) for map_to, map_from in mappings.items()],
                 polars.col(date_col).cast(polars.String).str.to_date('%Y%m%d').dt.weekday().alias('weekday')
             )
             .join(
                 polars.scan_csv(f'{source}/external.csv', separator=',', has_header=True, infer_schema_length=10000)
                 .select(polars.col([date_col, hour_col, *externals]))
-                .with_columns(polars.all().exclude([date_col, hour_col]).fill_nan(0)), 
+                .with_columns(polars.all().exclude([date_col, hour_col]).fill_nan(fill_nan)), 
                 on={date_col, hour_col}, how='inner'
             )
         ).filter(
